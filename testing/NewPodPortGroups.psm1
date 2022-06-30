e738cd695c3d6372cfd5d1b2adb7bb9c70d787d7
evanjd711 <evanjd711@gmail.com>
2022-06-30 15:08:06 -0700

    Created NewPodPortGroups

---
+++ testing/NewPodPortGroups.psm1
@@ -0,0 +1,49 @@
+<#
+Creates a specified amount of port groups using the lowest available numbers
+Author: Evan Deters
+#>
+
+function New-PodPortGroups {
+
+    param(
+        [ValidateRange(1,50)]
+        [Parameter(Mandatory=$true)]
+        [int] $Portgroups,
+        [Parameter(Mandatory=$true)]
+        [ValidateRange(1000,1299)]
+        [int] $StartPort,
+        [Parameter(Mandatory=$true)]
+        [ValidateRange(1001,1300)]
+        [int] $EndPort
+    )
+
+    $ErrorActionPreference = "Stop"
+
+    # Gets the list of existing port groups in the range
+    $PortGroupList = Get-VDPortgroup -VDSwitch Main_DSW | Select-Object -ExpandProperty name | Sort-Object
+    $PortGroupList = $PortGroupList | 
+        ForEach-Object {
+            [int]$PortGroupList[$PortGroupList.indexOf($_)].Substring(0, $PortGroupList[$PortGroupList.indexOf($_)].indexOf('_'))
+        }
+    $PortGroupList = $PortGroupList.where{$_ -IN $StartPort..$EndPort}
+    
+    # Check if Port Groups can be created
+    if($EndPort - $StartPort - $PortGroupList.Count + 1 -lt $Portgroups) {
+        $temp = $EndPort - $StartPort - $PortGroupList.Count + 1
+        Write-Error -Message "There are not enough port groups available in this range. Only $temp can be created."
+    }
+
+    # Creates the port groups
+    $j = $StartPort
+    $i = 0
+    While ($i -lt $Portgroups - 1) {
+        if($PortGroupList.IndexOf($j) -ne -1) { $j++; continue }
+        if($j -gt $EndPort) { Write-Error -Message "There are no more available port groups in the specified range."}
+        else {
+            $PodPortGroup = $j
+            New-VDPortgroup -VDSwitch Main_DSW -Name $PodPortGroup'_PodNetwork' -VlanId $PodPortGroup
+            $j++
+            $i++
+        }
+    }
+}
