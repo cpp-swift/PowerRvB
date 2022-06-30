d32b5910e6a6317e707a64d5b57132a0999763c3
evanjd711 <evanjd711@gmail.com>
2022-06-30 13:16:20 -0700

    Updated Password Generation

--- testing/NewPodUsers.psm1
+++ testing/NewPodUsers.psm1
@@ -15,7 +15,7 @@ function New-PodUsers {
     $users = @{}
     Import-Module ActiveDirectory
     for($i = 0; $i -lt $Pods.Length; $i++) {
-        $Password = Get-RandomPassword 12
+        $Password = Get-RandomPassword 12 1 1 1 1
         $Name = $Pods[$i] + '_User'
         $users.Add($Name, $Password)
         $Password = ConvertTo-SecureString -AsPlainText $Password -Force
@@ -30,19 +30,44 @@ function New-PodUsers {
 function Get-RandomPassword {
     param (
         [Parameter(Mandatory)]
-        [int] $length
+        [ValidateRange(4,[int]::MaxValue)]
+        [int] $length,
+        [int] $upper = 1,
+        [int] $lower = 1,
+        [int] $numeric = 1,
+        [int] $special = 1
     )
-    $charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*@$^%_!#?'.ToCharArray()
+    if($upper + $lower + $numeric + $special -gt $length) {
+        throw "number of upper/lower/numeric/special char must be lower or equal to length"
+    }
+    $uCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    $lCharSet = "abcdefghijklmnopqrstuvwxyz"
+    $nCharSet = "0123456789"
+    $sCharSet = "/*-+,!?=()@;:._"
+    $charSet = ""
+    if($upper -gt 0) { $charSet += $uCharSet }
+    if($lower -gt 0) { $charSet += $lCharSet }
+    if($numeric -gt 0) { $charSet += $nCharSet }
+    if($special -gt 0) { $charSet += $sCharSet }
+    
+    $charSet = $charSet.ToCharArray()
     $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
     $bytes = New-Object byte[]($length)
- 
     $rng.GetBytes($bytes)
  
     $result = New-Object char[]($length)
- 
     for ($i = 0 ; $i -lt $length ; $i++) {
-        $result[$i] = $charSet[$bytes[$i]%$charSet.Length]
+        $result[$i] = $charSet[$bytes[$i] % $charSet.Length]
     }
+    $password = (-join $result)
+    $valid = $true
+    if($upper   -gt ($password.ToCharArray() | Where-Object {$_ -cin $uCharSet.ToCharArray() }).Count) { $valid = $false }
+    if($lower   -gt ($password.ToCharArray() | Where-Object {$_ -cin $lCharSet.ToCharArray() }).Count) { $valid = $false }
+    if($numeric -gt ($password.ToCharArray() | Where-Object {$_ -cin $nCharSet.ToCharArray() }).Count) { $valid = $false }
+    if($special -gt ($password.ToCharArray() | Where-Object {$_ -cin $sCharSet.ToCharArray() }).Count) { $valid = $false }
  
-    return (-join $result)
+    if(!$valid) {
+         $password = Get-RandomPassword $length $upper $lower $numeric $special
+    }
+    return $password
 }
